1aa8fc7358d0be83152a72a56c5b4aa9b9e33a73
salim104 <salimshaban885@gmail.com>
2022-01-12 21:43:59 +0200

    font

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click',function(){
+    
     // console.log(links.classList);
     // console.log(links.classList.contains('random'));
     // console.log(links.classList.contains('links'));
